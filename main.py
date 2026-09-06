2da1594ae360fd8f1d0b261761fbc2f4cefd1008
UbiS213 <nikiforovnurgun83@gmail.com>
2026-09-06 18:09:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 # TODO: Добавить проверку на пустой ввод
 print("Моя первая программа")
 print("Второй коммит")
+from faker import Faker
+faker_instance = Faker()
+print("Случайное имя:", faker_instance.name())
+print("Случайный адрес:", faker_instance.address())
